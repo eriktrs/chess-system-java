01fb9f86f0851fbd7c8a26461aeedfe083d92c26
Eriktrs <erik.trs01@gmail.com>
2021-06-23 13:55:28 -0300

    First Class: Position

---
+++ src/boardgames/Position.java
@@ -0,0 +1,50 @@
+package boardgames;
+
+public class Position {
+	
+	//Defining row and column 
+	private int row;
+	private int column;
+	
+	
+	//Constructors
+	public Position() {
+	}
+	
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	//Gets and Setters
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	//Printing the position on screen
+	@Override
+	public String toString() {
+		return row + "," +column;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
