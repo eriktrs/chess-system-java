80c0e21d25cb64fd3b8f020e81883f4f82650470
Eriktrs <erik.trs01@gmail.com>
2021-06-24 15:43:43 -0300

    Little improvement in board printing

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -6,32 +6,45 @@ import chess.pieces.Rook;
 
 public class ChessMatch {
 	private Board board;
-	
+
 	public ChessMatch() {
-		//Setup the Chess Game
-		board = new Board(8,8);
+		// Setup the Chess Game
+		board = new Board(8, 8);
 		initialSetup();
 	}
-	
-	public ChessPiece[][] getPieces(){
-		//Creating auxiliary variable
+
+	public ChessPiece[][] getPieces() {
+		// Creating auxiliary variable
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
-		for (int i =0; i<board.getRows(); i++) {
-			for(int j=0; j<board.getColumns(); j++) {
-				//Downcasting 
+		for (int i = 0; i < board.getRows(); i++) {
+			for (int j = 0; j < board.getColumns(); j++) {
+				// Downcasting
 				mat[i][j] = (ChessPiece) board.piece(i, j);
 			}
 		}
 		return mat;
 	}
-	
+
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
-	
-	//Testing first setup
+
+	// Testing first setup
 	private void initialSetup() {
-		placeNewPiece('h', 8, new Rook(board, Color.WHITE));
-		placeNewPiece('e', 8, new King(board, Color.WHITE));
+		// White pieces
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 1, new King(board, Color.WHITE));
+		
+		// Black pieces
+		placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 8, new King(board, Color.BLACK));
 	}
 }
